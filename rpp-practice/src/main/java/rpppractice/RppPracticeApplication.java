fc6a3c81590903e2262ad398caee664a1dc3e51d
Miroslav Stefanovic <mstef@uns.ac.rs>
2019-03-03 19:30:52 +0100

    Initial project setup for RPP

---
+++ rpp-practice/src/main/java/rpppractice/RppPracticeApplication.java
@@ -0,0 +1,13 @@
+package rpppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RppPracticeApplication.class, args);
+	}
+
+}
